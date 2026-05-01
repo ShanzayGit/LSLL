cd900ae95196211ec9254adc925fa99466bb8178
Shanzay <malikshanzay135@gmail.com>
2026-05-01 15:17:54 +0500

    Update main.cpp

--- LSLL/main.cpp
+++ LSLL/main.cpp
@@ -18,8 +18,8 @@ int main()
 	a.insertAtTail("BLACK");
 	a.insertAtTail("BLACK");
 
-	cout << a.findMajorityColor(a, 6);
-	cout << "\n"; a.print();
+	cout << a.findMajorityColor(a, 6);//WHITE
+	cout << "\n"; a.print();// WHITE WHITE WHITE WHITE WHITE WHITE BLACK BLACK BLACK BLACK
 	cout << "\n";
 
 	LSLL<int>  b, c,d;
@@ -31,13 +31,13 @@ int main()
 	c.insertAtTail(1);
 	c.insertAtTail(5);
 	c.insertAtTail(7);
-	b.print();
+	b.print(); //2 3 5 6 
 	cout << '\n';
-	c.print();
+	c.print();//1 5 7
 	cout << '\n';
 	d=b.mergeSortedList(c);
-	d.print();
+	d.print(); //1 2 3 5 5 6 7
 	d.removeDuplicates();
 	cout << '\n';
-	d.print();
+	d.print(); // 1 2 3 5 6 7 
 }
